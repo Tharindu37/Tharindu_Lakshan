3eac8779408436bff54d4e863c2ff3de619875ee
Tharindu37 <tharindut520@gmail.com>
2023-01-15 19:48:07 +0530

    Added auto typing perchance

---
+++ js/script.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    console.log("lk")
+    $('#intro').typed({
+        strings: [
+            "A full stack developer is a software engineer who has knowledge and experience in all layers of software development, including the front-end (user interface), back-end (server-side logic), and database. They have the ability to work on both client-side and server-side development and can handle the complete software development cycle, from concept to deployment."
+        ],
+        typeSpeed: 5,
+        contentType: 'html'
+    });
+}, 500);
